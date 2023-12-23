42d1de1f1d22ecfea2c96f8ebeb37d6ce95c36fb
Hitesh A H <hitesh.harikumar@teranet.ca>
2023-12-23 20:24:07 +0530

    Aspect- Around Loggable

--- Spring-Framework/SpringFrameWork/src/main/java/com/teranet/rps/springtraining/aspect/LoggingAspect.java
+++ Spring-Framework/SpringFrameWork/src/main/java/com/teranet/rps/springtraining/aspect/LoggingAspect.java
@@ -1,10 +1,8 @@
 package com.teranet.rps.springtraining.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -37,24 +35,52 @@ public class LoggingAspect {
     // LOGGER.info(logMessage.toString());
     // }
 
-    @AfterReturning(value = "executeLogging()", returning = "returnValue")
-    public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
+    // @AfterReturning(value = "executeLogging()", returning = "returnValue")
+    // public void logMethodCall(JoinPoint joinPoint, Object returnValue) {
+    // StringBuilder message = new StringBuilder("Method: ");
+    // message.append(joinPoint.getSignature().getName());
+    // Object[] args = joinPoint.getArgs();
+    // if (null != args && args.length > 0) {
+    // message.append(" args=[ | ");
+    // Arrays.asList(args).forEach(
+    // arg -> {
+    // message.append((arg)).append(" | ");
+    // });
+    // }
+    // if (returnValue instanceof Collection) {
+    // message.append(", returning: ").append(((Collection<?>)
+    // returnValue).size()).append(" instance(s)");
+    // } else {
+    // message.append(",returning: ").append(returnValue.toString());
+    // }
+    // LOGGER.info(message.toString());
+    // }
+
+    @Around(value = "executeLogging()")
+    public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object returnValue = joinPoint.proceed();
+        long totalTime = System.currentTimeMillis()-startTime;
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
+        message.append(" totalTime: ").append(totalTime).append("ms");
         Object[] args = joinPoint.getArgs();
-        if (null != args && args.length > 0) {
+        if (null!=args && args.length>0){
             message.append(" args=[ | ");
-            Arrays.asList(args).forEach(
-                    arg -> {
-                        message.append((arg)).append(" | ");
-                    });
+            Arrays.asList(args).forEach(arg->{
+                message.append(arg).append(" | ");
+            });
+            message.append("]");
         }
-        if (returnValue instanceof Collection) {
-            message.append(", returning: ").append(((Collection<?>) returnValue).size()).append(" instance(s)");
-        } else {
-            message.append(",returning: ").append(returnValue.toString());
+        if(returnValue instanceof Collection){
+            message.append(", returning: ").append(((Collection)returnValue).size()).append(" instance(s)");
+        }else{
+            message.append(", returning: ").append(returnValue.toString());
         }
+
         LOGGER.info(message.toString());
+        return returnValue;
     }
+    
 
 }
