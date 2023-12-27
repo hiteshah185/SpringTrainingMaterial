00b183dfa6004174d34b470c3104b72cd94dbbd6
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2023-12-27 17:24:14 +0530

    API Endpoints for Employee

--- Spring-Design-Patterns/SpringDesignPatterns/src/main/java/com/teranet/rps/springtraining/designpatterns/controller/EmployeeController.java
+++ Spring-Design-Patterns/SpringDesignPatterns/src/main/java/com/teranet/rps/springtraining/designpatterns/controller/EmployeeController.java
@@ -1,22 +1,55 @@
 package com.teranet.rps.springtraining.designpatterns.controller;
 
+import com.teranet.rps.springtraining.designpatterns.builder.ContactCard;
+import com.teranet.rps.springtraining.designpatterns.builder.ContactCardBuilder;
+import com.teranet.rps.springtraining.designpatterns.models.Employee;
 import com.teranet.rps.springtraining.designpatterns.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = {"*"})
-@RequestMapping("/api/employee/")
+@RequestMapping("/api/")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    RestTemplate restTemplate;
     @GetMapping("testPoint")
     public ResponseEntity getResponse(){
         return employeeService.getNewEmployee();
     }
+    @GetMapping("employees")
+    public List<Employee> getEmployeesList(){
+        List<Employee> employees = new ArrayList<>();
+        Employee newEmployee =  new Employee();
+        newEmployee.setFirstName("Kailas");
+        newEmployee.setEmail("kailas@mail.com");
+        newEmployee.setDomain("java");
+        employees.add(newEmployee);
+        return employees;
+    }
+    @GetMapping("employee/{id}")
+    public ResponseEntity getEmployeeById(@PathVariable Long id){
+        return employeeService.getEmployeeFromId(id);
+    }
+
+    @GetMapping("employeeContact/{id}")
+    public ContactCard getEmployeeContactById(@PathVariable Long id){
+        Employee employee = this.restTemplate
+                .getForEntity("http://localhost:8080/api/employee/{id}",
+                        Employee.class,id).getBody();
+        return (new ContactCardBuilder()
+                .setFirstName(employee.getFirstName())
+                .setWorkEmail(employee.getEmail())
+                .setDepartment("Development").buildContactCard());
+    }
+
+
 }
--- Spring-Design-Patterns/SpringDesignPatterns/src/main/java/com/teranet/rps/springtraining/designpatterns/service/EmployeeService.java
+++ Spring-Design-Patterns/SpringDesignPatterns/src/main/java/com/teranet/rps/springtraining/designpatterns/service/EmployeeService.java
@@ -1,13 +1,26 @@
 package com.teranet.rps.springtraining.designpatterns.service;
 
 import com.teranet.rps.springtraining.designpatterns.models.Employee;
+import com.teranet.rps.springtraining.designpatterns.repo.EmployeeRpo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class EmployeeService {
+    @Autowired
+    private EmployeeRpo employeeRpo;
     public ResponseEntity getNewEmployee(){
         return new ResponseEntity<>(new Employee(11,"Kevin","kevin@mail.com","C#"), HttpStatus.OK);
+    }
+    public ResponseEntity getEmployeeFromId(Long id){
+       Optional <Employee> employee = employeeRpo.findById(id);
+        if(employee.isPresent()){
+            return new ResponseEntity<Employee>( employee.get(),HttpStatus.OK);
+        }
+        return  new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 }
