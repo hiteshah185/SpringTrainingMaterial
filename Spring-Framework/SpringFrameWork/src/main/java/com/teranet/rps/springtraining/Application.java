5e4611031101bafc3bba5b9b059b987c4eb14a99
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-01-02 10:10:39 +0530

    BeanFactory & XML Based Configurations

--- Spring-Framework/SpringFrameWork/src/main/java/com/teranet/rps/springtraining/Application.java
+++ Spring-Framework/SpringFrameWork/src/main/java/com/teranet/rps/springtraining/Application.java
@@ -1,16 +1,24 @@
 package com.teranet.rps.springtraining;
 
+import com.teranet.rps.springtraining.beans.Pen;
 import com.teranet.rps.springtraining.config.ApplicationConfig;
 import com.teranet.rps.springtraining.service.OutputService;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 public class Application {
     public static void main(String[] args) throws Exception{
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        ApplicationContext contextForUSA = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));//XML based
+        Pen myCurrentPen = (Pen)factory.getBean("pen");
         OutputService outputService = context.getBean(OutputService.class);
         System.out.println("<---Teranet RPS Spring Training: Main Class --->");
         outputService.getOutput();
+        myCurrentPen.write();
 
     }
 }
---
+++ Spring-Framework/SpringFrameWork/src/main/java/com/teranet/rps/springtraining/beans/Pen.java
@@ -0,0 +1,53 @@
+package com.teranet.rps.springtraining.beans;
+
+public class Pen {
+    private String company;
+    private String color;
+    private float price;
+    private float weight;
+
+    public Pen(String company, String color, float price, float weight) {
+        this.company = company;
+        this.color = color;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public Pen() {
+    }
+    public void write(){
+        System.out.println("Writing with pen....");
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
